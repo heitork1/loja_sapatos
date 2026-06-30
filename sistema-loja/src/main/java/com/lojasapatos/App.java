59c33d44f8725e138563c0044bbc164fd456b759
Leandro Luciano Peters <leandrolucianopeters@yahoo.com.br>
2026-06-30 17:21:58 -0300

    Update App.java

--- sistema-loja/src/main/java/com/lojasapatos/App.java
+++ sistema-loja/src/main/java/com/lojasapatos/App.java
@@ -1,23 +1,25 @@
 package com.lojasapatos;
 
+import com.lojasapatos.dao.Conexao;
+import com.lojasapatos.ui.login.LoginFrame;
+
+import javax.swing.*;
 import java.sql.Connection;
 
-import com.lojasapatos.dao.Conexao;
-
-/**
- * Hello world!
- *
- */
 public class App {
+
     public static void main(String[] args) {
         Connection c = Conexao.getConexao();
-
         if (c != null) {
             System.out.println("O sistema está online e conectado ao Postgres!");
+            try { c.close(); } catch (Exception ignored) {}
         } else {
             System.out.println("O sistema está offline. Verifique o banco de dados.");
         }
-        
-        System.out.println("Hello World!");
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ignored) {}
+
+        SwingUtilities.invokeLater(() -> new LoginFrame().setVisible(true));
     }
 }
