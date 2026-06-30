37937d5e9c82f65717d8a774b010c5963a74b7f0
Leandro Luciano Peters <leandrolucianopeters@yahoo.com.br>
2026-06-30 17:28:46 -0300

    Create ItemVenda.java

---
+++ sistema-loja/src/main/java/com/lojasapatos/model/ItemVenda.java
@@ -0,0 +1,33 @@
+package com.lojasapatos.model;
+
+import java.math.BigDecimal;
+
+public class ItemVenda {
+    private Integer idVenda;
+    private Integer codigo;
+    private Integer quantidade;
+    private BigDecimal precoUnitario;
+
+    public ItemVenda() {}
+    public ItemVenda(Integer idVenda, Integer codigo, Integer quantidade, BigDecimal precoUnitario) {
+        this.idVenda = idVenda;
+        this.codigo = codigo;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+    }
+
+    public Integer getIdVenda() { return idVenda; }
+    public void setIdVenda(Integer idVenda) { this.idVenda = idVenda; }
+    public Integer getCodigo() { return codigo; }
+    public void setCodigo(Integer codigo) { this.codigo = codigo; }
+    public Integer getQuantidade() { return quantidade; }
+    public void setQuantidade(Integer quantidade) { this.quantidade = quantidade; }
+    public BigDecimal getPrecoUnitario() { return precoUnitario; }
+    public void setPrecoUnitario(BigDecimal precoUnitario) { this.precoUnitario = precoUnitario; }
+
+    @Override
+    public String toString() {
+        return "ItemVenda{idVenda=" + idVenda + ", codigo=" + codigo + ", quantidade=" + quantidade +
+               ", precoUnitario=" + precoUnitario + "}";
+    }
+}
