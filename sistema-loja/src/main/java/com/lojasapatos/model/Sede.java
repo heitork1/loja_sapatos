fcbda8298d95b650bd3cd0eca1c5bd8e3eecebc2
Leandro Luciano Peters <leandrolucianopeters@yahoo.com.br>
2026-07-01 12:34:37 -0300

    Update Sede.java

--- sistema-loja/src/main/java/com/lojasapatos/model/Sede.java
+++ sistema-loja/src/main/java/com/lojasapatos/model/Sede.java
@@ -4,6 +4,8 @@ public class Sede {
     private Integer idSede;
     private String nomeSede;
     
+    public Sede() {}
+    public Sede(String nomeSede) { this.nomeSede = nomeSede; }
     public Sede(Integer idSede, String nomeSede) {
         this.idSede = idSede;
         this.nomeSede = nomeSede;
@@ -24,6 +26,10 @@ public class Sede {
     public void setNomeSede(String nomeSede) {
         this.nomeSede = nomeSede;
     }
-
+    
+    @Override
+    public String toString() {
+        return "Sede{idSede=" + idSede + ", nomeSede='" + nomeSede + "'}";
+    }
     
 }
