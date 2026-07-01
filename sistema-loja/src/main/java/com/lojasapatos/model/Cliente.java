dcc9d94c53fffe9bb0f94f3fd6a2adb5e36885a8
Leandro Luciano Peters <leandrolucianopeters@yahoo.com.br>
2026-07-01 12:24:57 -0300

    Update Cliente.java

--- sistema-loja/src/main/java/com/lojasapatos/model/Cliente.java
+++ sistema-loja/src/main/java/com/lojasapatos/model/Cliente.java
@@ -4,72 +4,50 @@ import java.time.LocalDate; // Importante para datas!
 
 public class Cliente {
     private String cpf;
+    private String nome;
+    private LocalDate dataNascimento;
     private String telefone;
     private String email;
-    private int pontos;
-
-    
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public int getPontos() {
-        return pontos;
-    }
-
-    public void setPontos(int pontos) {
-        this.pontos = pontos;
-    }
-
-    public LocalDate getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(LocalDate dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
-    public String getRua() {
-        return rua;
-    }
-
-    public void setRua(String rua) {
-        this.rua = rua;
-    }
-    private String nome;
-    private LocalDate dataNascimento; // Em Java usamos LocalDate para datas
-    private String numero;
     private String rua;
+    private String numero;
+    private String cep;
+    private Integer pontos;
 
     public Cliente() {}
 
-    public Cliente(String cpf, String telefone, String email, int pontos, String nome, LocalDate dataNascimento, String numero, String rua) {
+    public Cliente(String cpf, String nome, LocalDate dataNascimento, String telefone,
+                    String email, String rua, String numero, String cep, Integer pontos) {
         this.cpf = cpf;
-        this.telefone = telefone;
-        this.email = email;
-        this.pontos = pontos;
         this.nome = nome;
         this.dataNascimento = dataNascimento;
-        this.numero = numero;
+        this.telefone = telefone;
+        this.email = email;
         this.rua = rua;
+        this.numero = numero;
+        this.cep = cep;
+        this.pontos = pontos;
     }
 
     public String getCpf() { return cpf; }
     public void setCpf(String cpf) { this.cpf = cpf; }
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
+    public LocalDate getDataNascimento() { return dataNascimento; }
+    public void setDataNascimento(LocalDate dataNascimento) { this.dataNascimento = dataNascimento; }
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
-}
+    public String getRua() { return rua; }
+    public void setRua(String rua) { this.rua = rua; }
+    public String getNumero() { return numero; }
+    public void setNumero(String numero) { this.numero = numero; }
+    public String getCep() { return cep; }
+    public void setCep(String cep) { this.cep = cep; }
+    public Integer getPontos() { return pontos; }
+    public void setPontos(Integer pontos) { this.pontos = pontos; }
+
+    @Override
+    public String toString() {
+        return "Cliente{cpf='" + cpf + "', nome='" + nome + "', pontos=" + pontos + "}";
+    }
